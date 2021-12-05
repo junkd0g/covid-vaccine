3782e5110c150d78b462bbc34c8d3255b11a5a70
Iordanis Paschalidis <iordanis@ipaschalidis.com>
2021-12-05 10:04:24 +0000

    more unit tests

--- internal/vaccine/vaccine_test.go
+++ internal/vaccine/vaccine_test.go
@@ -59,19 +59,91 @@ func Test_CountryData(t *testing.T) {
 		assert.Contains(t, expectedError, err.Error())
 	})
 
-	t.Run("CountryData runs successfully", func(t *testing.T) {
-		expectedError := "data_ReadData_open_file"
+	t.Run("CountryData runs successfully but empty response", func(t *testing.T) {
+		m := getMocks(t)
+		var dd []data.Country
+
+		el := data.Country{
+			Name:                            "Greece",
+			PeopleVaccinations:              []int{3, 4, 5, 6, 7, 88},
+			TotalBoostersPerHundred:         "17",
+			TotalVaccinations:               []int{3, 4, 5, 6, 7, 88},
+			PeopleVaccinatedPerHundred:      "83",
+			TotalVaccinationsPerHundred:     "232",
+			TotalBooster:                    []int{3, 4, 5, 6, 7, 88},
+			PeopleFullyVaccinations:         []int{3, 4, 5, 6, 7, 88},
+			PeopleFullyVaccinatedPerHundred: "322",
+		}
+
+		dd = append(dd, el)
+
 		m.rd.
 			EXPECT().
 			ReadData().
-			Return(data.Countries{}, fmt.Errorf("%s", expectedError)).
-			AnyTimes()
+			Return(data.Countries{
+				Data: dd,
+			}, nil)
 
 		client, err := vaccine.NewClient(m.rd)
 		assert.Nil(t, err)
 
-		_, err = client.CountryData("some country")
-		assert.NotNil(t, err)
-		assert.Contains(t, expectedError, err.Error())
+		resp, err := client.CountryData("some country")
+		assert.Nil(t, err)
+		assert.Equal(t, vaccine.CountryDataResponse{}, resp)
+	})
+
+	t.Run("CountryData runs successfully with a response", func(t *testing.T) {
+		m := getMocks(t)
+		var dd []data.Country
+
+		el := data.Country{
+			Name:                            "Greece",
+			PeopleVaccinations:              []int{3, 4, 5, 6, 7, 324},
+			TotalBoostersPerHundred:         "17",
+			TotalVaccinations:               []int{3, 4, 5, 6, 7, 88},
+			PeopleVaccinatedPerHundred:      "83",
+			TotalVaccinationsPerHundred:     "232",
+			TotalBooster:                    []int{3, 4, 5, 6, 7, 122},
+			PeopleFullyVaccinations:         []int{3, 4, 5, 6, 7, 432},
+			PeopleFullyVaccinatedPerHundred: "322",
+		}
+
+		el2 := data.Country{
+			Name:                            "Portugal",
+			PeopleVaccinations:              []int{3, 4, 5, 6, 7, 1324},
+			TotalBoostersPerHundred:         "117",
+			TotalVaccinations:               []int{3, 4, 5, 6, 7, 188},
+			PeopleVaccinatedPerHundred:      "183",
+			TotalVaccinationsPerHundred:     "232",
+			TotalBooster:                    []int{3, 4, 5, 6, 7, 1122},
+			PeopleFullyVaccinations:         []int{3, 4, 5, 6, 7, 1432},
+			PeopleFullyVaccinatedPerHundred: "1322",
+		}
+
+		dd = append(dd, el)
+		dd = append(dd, el2)
+
+		m.rd.
+			EXPECT().
+			ReadData().
+			Return(data.Countries{
+				Data: dd,
+			}, nil)
+
+		client, err := vaccine.NewClient(m.rd)
+		assert.Nil(t, err)
+
+		resp, err := client.CountryData("Greece")
+		assert.Nil(t, err)
+		extectedResponse := vaccine.CountryDataResponse{
+			TotalVaccinations:               88,
+			PeopleFullyVaccinations:         432,
+			TotalBooster:                    122,
+			TotalVaccinationsPerHundred:     "232",
+			PeopleVaccinatedPerHundred:      "83",
+			PeopleFullyVaccinatedPerHundred: "322",
+			TotalBoostersPerHundred:         "17",
+		}
+		assert.Equal(t, extectedResponse, resp)
 	})
 }
